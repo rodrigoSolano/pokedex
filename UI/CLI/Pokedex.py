53d165b2b30966d938375c9120a3d26bb5173d51
rodrigoSolano <rodrigosolano865@gmail.com>
2021-11-10 19:39:39 -0600

    refactor: Handling errors when selecting an option from the main menu.

--- UI/CLI/Pokedex.py
+++ UI/CLI/Pokedex.py
@@ -11,23 +11,38 @@ class Pokedex:
     def showMenu(self):
         print("Welcome to 'THE POKEDEX'")
         while True:
+            pokemon = None
             print("1. Get pokemon by name")
             print("2. Get pokemon by id")
             print("3. Exit")
             print("Please enter a number: ", end="")
             choice = input()
-            if choice == "1":
+            try:
+                choice = int(choice)
+            except ValueError:
+                print("Invalid choice")
+                print("")
+                continue
+            if choice == 1:
                 print("Please enter a pokemon name: ", end="")
                 pokemon_name = input()
                 pokemon = self.getPokemonByName(pokemon_name)
-                self.showPokemonInfo(pokemon)
-            elif choice == "2":
+            elif choice == 2:
                 print("Please enter a pokemon id: ", end="")
                 pokemon_id = int(input())
                 pokemon = self.getPokemonById(pokemon_id)
-                self.showPokemonInfo(pokemon)
-            elif choice == "3":
+            elif choice == 3:
                 break
+            else:
+                print("Invalid choice")
+                print("")
+                continue
+            
+            if pokemon is not None:
+                self.showPokemonInfo(pokemon)                    
+            else:
+                print("Pokemon not found")
+                
             print("")
 
     def showPokemonInfo(self,pokemon:BasePokemon):
