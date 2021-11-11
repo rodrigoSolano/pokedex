799f4484f7add5b2eb3e3688bd59b9cea99d83af
rodrigoSolano <rodrigosolano865@gmail.com>
2021-11-10 19:36:34 -0600

    refactor: Refactoring in the way of requesting the API data.

--- repository/PokemonAPIRepository.py
+++ repository/PokemonAPIRepository.py
@@ -12,32 +12,20 @@ class PokemonAPIRepository(PokemonRepository):
         super().__init__()
 
     def getPokemonById(self, id: int) -> Pokemon:
-        pokemon_data = requests.get(self.baseUrl + str(id))
-        pokemon_data = pokemon_data.json()
-        pokemon_name =  pokemon_data["name"]
-        pokemon_id = pokemon_data["id"]
-
-        pokemon_types = []
-
-        for type in pokemon_data["types"]:
-            pokemon_types.append(type["type"]["name"])
-
-
-        pokemon = PokemonFactory.makePokemon(pokemon_id, pokemon_name, pokemon_types)
-
-        return pokemon
+        return self.getPokemonData(self.baseUrl + str(id))
 
     def getPokemonByName(self, name: str) -> Pokemon:
-        pokemon_data = requests.get(self.baseUrl + name)
-        pokemon_data = pokemon_data.json()
-        pokemon_name =  pokemon_data["name"]
-        pokemon_id = pokemon_data["id"]
+        return self.getPokemonData(self.baseUrl + name)
 
-        pokemon_types = []
-
-        for type in pokemon_data["types"]:
-            pokemon_types.append(type["type"]["name"])
-
-        pokemon = PokemonFactory.makePokemon(pokemon_id, pokemon_name, pokemon_types)
-
-        return pokemon
+    def getPokemonData(self, url: str) -> Pokemon:
+        pokemon_data = requests.get(url)
+        if pokemon_data:
+            pokemon_data = pokemon_data.json()
+            pokemon_name =  pokemon_data["name"]
+            pokemon_id = pokemon_data["id"]
+            pokemon_types = []
+            for pokemon_type in pokemon_data["types"]:
+                pokemon_types.append(pokemon_type["type"]["name"])
+            pokemon = PokemonFactory.makePokemon(pokemon_id, pokemon_name, pokemon_types)
+            return pokemon
+        else:
